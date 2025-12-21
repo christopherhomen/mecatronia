aca9570cdb47ac43dfca64c14a1cd0c1b70a6c53
Christopher Homen <chris.a.d.97@gmail.com>
2025-12-21 15:41:40 -0500

    System: Actualizar Service Worker (Cache) para MecaTronia

--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
-const CACHE_NAME = 'serviauto-v1';
+const CACHE_NAME = 'mecatronia-v1';
 const ASSETS = [
     './',
     './index.html',
     './styles.css',
+    './sync-styles.css',
     './app.js',
+    './logo.PNG',
+    './vistas.PNG',
     'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Oswald:wght@500;700&display=swap'
 ];
 
